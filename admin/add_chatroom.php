1285ef80d54221e923fc6f449bd4c4ff300add2d
yashagarwal1301 <yashagarwal1301@gmail.com>
2020-12-08 12:04:29 +0530

    Update add_chatroom.php

--- admin/add_chatroom.php
+++ admin/add_chatroom.php
@@ -5,14 +5,10 @@
 	$cid="";
 	$chat_name=$_POST['chatname'];
 	$chat_password=$_POST['chatpass'];
-	
-	mysqli_query($conn,"insert into chatroom (chat_name, chat_password, date_created, userid) values ('$chat_name', '$chat_password', NOW(), '".$_SESSION['id']."')");
-	$cid=mysqli_insert_id($conn);
+	mysqli_query($conn,"insert into chatroom (chat_name, chat_password, date_created, userid) values ('$chat_name', '$chat_password', NOW(), '".$_SESSION['id']."')")
 	
 	mysqli_query($conn,"insert into chat_member (chatroomid, userid) values ('$cid', '".$_SESSION['id']."')");
 	
-	echo $cid;
-	}
-	
+	echo $cid	
 	
 ?>
